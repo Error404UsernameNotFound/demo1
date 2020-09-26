ec6f372f95813c34b99eb132639491beceb7eb85
Raúl M. <60326859+Error404UsernameNotFound@users.noreply.github.com>
2020-09-26 03:38:41 +0200

    Cambios en cambio3

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -1,10 +1,12 @@
 package demo1;
 
 public class Demo {
+	//Cambio 1 para el rebase
 	public static void main(String[] args) {
+		//Segundo cambio de rebase
 		System.out.println("linea 1");
 		System.out.println("cambio 3 en cambio3 DEFINITIVO");
 		System.out.println("linea 3");
 		//Cambio desde la nueva rama (cambio2)
-	}
+	} //Tercer cambio de rebase
 }
